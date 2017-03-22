ea4c46c0d6956991db6b0ebfd93ebfc401d2e527
manish.gautam.india <manish.gautam.india@gmail.com>
2017-03-22 08:07:00 -0400

    JS Changes

--- dist/js/customfunctions.js
+++ dist/js/customfunctions.js
@@ -1,8 +1,20 @@
 $.getJSON("js/data.json", function (data) {
   var items = [];
-  $.each(data, function (key, val) {
-    items.push("<li id='" + key + "'>" + val + "</li>");
+  
+  var navbar = "";
+  
+  
+  /*<ul>
+    <li><a onclick="add();"><i class="fa fa-facebook"></i> Facebook</a></li>
+  </ul>*/
+  
+                            
+  $.each(data.node, function (key, val) {
+    navbar += '<li><a onclick="add();"><i class="fa fa-share-alt"></i><span>'+ val.name +'</span><span class="fa fa-angle-right" style="float: right"></span></a>';
+    navbar += '</li>';
   });
+  
+  console.log(navbar);
 
   var nav = $("<ul/>", {
     "class": "my-new-list",
